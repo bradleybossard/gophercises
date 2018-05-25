9264b8a35ee6acffe26234087bc3caaaa290a166
Bradley Bossard <bradleybossard@gmail.com>
2018-05-25 09:58:12 -0500

    Complete first part of lesson

--- exercise-1/main.go
+++ exercise-1/main.go
@@ -4,6 +4,7 @@ import "encoding/csv"
 import "fmt"
 import "flag"
 import "os"
+import "strings"
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file of questions and answers")
@@ -20,7 +21,35 @@ func main() {
 	if err != nil {
 		exit("Failed to parse csv file")
 	}
-	fmt.Println(lines)
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s\n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d correct\n", correct, len(problems))
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: strings.TrimSpace(line[0]),
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+
+	return ret
 }
 
 func exit(msg string) {
